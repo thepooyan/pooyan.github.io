65b83752065a11d023311bbb531e8bd9e456ede8
Pooyan <thepooyan@gmail.com>
2022-10-06 12:00:03 +0330

    update

--- app.js
+++ app.js
@@ -31,6 +31,39 @@ $(function () {
     window.dc = dc;
   })()
 
+  function getOffsetTop(element) {
+    return element ? (element.offsetTop + getOffsetTop(element.offsetParent)) : 0;
+  }
+
+  function doSmIfCompIsSeen(comp, callback, offset = 400) {
+
+    function isVisible(query, expand = 0) {
+      let elements = dc.queries(query);
+
+      return function () {
+        let res = Object.values(elements).find(item => {
+          return (window.scrollY + window.innerHeight > getOffsetTop(item) - expand && window.scrollY < getOffsetTop(item) + item.clientHeight + expand)
+        });
+        if (res) return res; else return false;
+      }
+    }
+    const isCompVisible = isVisible(comp, offset);
+
+    function compLoader() {
+      if (isCompVisible()) {
+        callback(isCompVisible());
+        window.removeEventListener('scroll', compLoader);
+      }
+    }
+
+    window.addEventListener('scroll', compLoader);
+    compLoader()
+  }
+
+  doSmIfCompIsSeen('.pad', (item)=>{
+    item.classList.add('active')
+  }, -500)
+
   //* onclick system
   refreshOnClicks();
   function refreshOnClicks() {
